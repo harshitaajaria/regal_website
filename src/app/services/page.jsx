b46f0c2718f99dfde310a351cb7848133abaa4c2
anshikaaaabapna <anshikabapna4@gmail.com>
2023-10-10 15:53:26 +0530

    created service page

---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h2 className='py-12 text-center bg-amber-300 text-3xl'>this is services page</h2>
+    </div>
+  )
+}
+
+export default page
