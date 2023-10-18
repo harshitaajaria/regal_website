b475965baadfa9b8caf03ee9a779349251e3e452
harshitaajaria <harshitaajaria@gmail.com>
2023-10-18 13:40:09 +0530

    renamed js to jsx main file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-<h2 className='text-4xl text-red-500'>this is regal outsourse website</h2>
+      <h2 className="text-4xl text-red-500">this is regal outsourse website</h2>
     </main>
-  )
+  );
 }
